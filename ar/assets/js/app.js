2f09946c4840231a7a7eeb9b6e9cd8be9681d05b
Taha Azam <muhammadtahaazam@gmail.com>
2021-02-24 14:55:56 +0500

    Infinity web pages

---
+++ ar/assets/js/app.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("mySidenav").style.display = "block";
+
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.display = "none";
+  }
